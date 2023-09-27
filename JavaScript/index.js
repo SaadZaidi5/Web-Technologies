06a6e516ccb91dcea72664b398411cde9b8de4f8
SaadZaidi5 <saadzaidi169@gmail.com>
2023-09-27 16:24:59 +0500

    Task 1

---
+++ JavaScript/index.js
@@ -0,0 +1,13 @@
+
+let markWeight = 78;
+let markHeight = 1.69;
+let johnWeight = 92;
+let johnHeight = 1.95;
+let markBMI = markWeight/markHeight*markHeight;
+let johnBMI = johnWeight/johnHeight*johnHeight;
+let greaterBMI = false;
+if( markBMI > johnBMI)
+{
+    greaterBMI = true;
+}
+console.log( greaterBMI);
