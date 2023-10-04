6627ede2a4130410b6df4e0d5737739d7eff6e2b
SaadZaidi5 <saadzaidi169@gmail.com>
2023-10-05 00:13:50 +0500

    Worked on Files

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,9 +1,24 @@
 function disappear(){
-      const div=document.getElementById('div')
-      const child = div.querySelector('#paragraph');
-      child.style.display="none";
-      const para = document.createElement("h3");
-      para.innerText = "This is a heading.";
-      div.appendChild(para);
+      const div=document.getElementById('container')
+      div.style.display="none";
       
     }
+ 
+function inputData()
+{
+    var title = document.getElementById('title').value;
+    var author = document.getElementById('author').value;
+    const first = document.createElement('p');
+    const second = document.createElement('p');
+    const button = document.createElement('button')
+    button.innerText="Remove"
+    button.setAttribute("onclick", "disappear()")
+
+    first.innerText= title;
+    second.innerText=author;
+    const div=document.getElementById('container')
+    div.appendChild(first);
+    div.appendChild(second);
+    div.appendChild(button);
+
+}
